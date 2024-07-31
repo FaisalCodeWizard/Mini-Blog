35712341b0c1800066b7e3420bf38bfe74b90e3b
FaisalCodeWizard <faisalfarooqui077@gmail.com>
2024-07-31 17:16:24 +0530

    Jinja templates and config URL

--- Tweet/views.py
+++ Tweet/views.py
@@ -22,7 +22,7 @@ def tweet_create(request):
             return redirect('tweet_list')
     else:
         form = TweetForm()
-    return render(request, 'tweet_form.html', {'form': 'form'})
+    return render(request, 'tweet_form.html', {'form': form})
 
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
@@ -36,12 +36,13 @@ def tweet_edit(request, tweet_id):
         pass
     else:
         form = TweetForm(instance=tweet)
-    return render(request, 'tweet_form.html', {'form': 'form'})
+    return render(request, 'tweet_form.html', {'form': form})
 
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
     if request.method == 'POST':
+        tweet.delete()
         return redirect('tweet_list')
-    return render(request, 'tweet_confirm_delete.html', {'tweet': 'tweet'})
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
 
 
